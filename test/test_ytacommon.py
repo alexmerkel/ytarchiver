f8f1e892ff72d4806cbcab32dd4391e197e1bdd0
Alex Merkel <mail@alexmerkel.com>
2021-01-09 18:30:01 +0100

    Add test for duration conversion

--- test/test_ytacommon.py
+++ test/test_ytacommon.py
@@ -22,6 +22,16 @@ def setup():
 # ########################################################################### #
 
 # --------------------------------------------------------------------------- #
+@pytest.mark.parametrize("string,expected", [("PT8S", 8), ("PT34S", 34), ("PT3M14S", 194), ("PT28M31S", 1711), ("PT1H28M55S", 5335), ("PT1H16S", 3616), ("P4DT4H1S", 360001), ("P1DT36M41S", 88601), ("P2W3DT13H25M19S", 1517119)], ids=["sec", "two_sec", "sec+min", "sec+two_min", "sec+min+hour", "sec+hour", "day+hour+sec", "day+min+sec", "week+day+hour+min+sec"])
+def test_convertDuration(string, expected):
+    '''Test the duration conversion'''
+    #Convert
+    received = ytacommon.convertDuration(string)
+    #Compare
+    assert received == expected
+# ########################################################################### #
+
+# --------------------------------------------------------------------------- #
 @pytest.mark.parametrize("num", [1, 2, 3, 4, 5, 6], ids=["default", "only_two", "unicode", "split_second", "split_first", "formats"])
 def test_extractChapters(num):
     '''Test the chapter extraction'''
