3e0fdcf88a7f91f87732beaad9f03707efa72bd5
Alex Merkel <mail@alexmerkel.com>
2021-01-11 10:15:39 +0100

    Add database and temp copy fixtures

--- test/conftest.py
+++ test/conftest.py
@@ -2,6 +2,10 @@
 ''' configure pytest run '''
 
 import os
+import sqlite3
+import string
+import random
+from shutil import copyfile
 import pytest
 from appdirs import user_data_dir
 
@@ -61,11 +65,58 @@ def apikey(capsys):
     #Ask user
     if not apiKey:
         with capsys.disabled():
-            print("INFO: No testing API key available!")
+            print("ERROR: No testing API key available!")
             print("INFO: Add it to the \'{}\' file,".format(apiFile))
             print("INFO: to the $YTA_TEST_APIKEY environment variable,")
             print("INFO: or use \'-m \"not tube\"\' option to skip tests that require an API key")
         pytest.exit("Missing Youtube API key for testing", 3)
     #Save result
     os.environ["YTA_TEST_APIKEY"] = apiKey
+# ########################################################################### #
+
+# --------------------------------------------------------------------------- #
+@pytest.fixture
+def tempcopy(request):
+    '''Creates a temporary copy of the file given via the "internal_path"
+    marker, rewrites the "internal_path" to the new temporary file and
+    delete it afterwards
+    '''
+    #Read path
+    origPath = request.node.get_closest_marker("internal_path").args[0]
+    #Generate temporary path
+    _, ext = os.path.splitext(origPath)
+    tempPath = os.path.join(os.environ["YTA_TESTDATA"], "temp_"+generateRandom(10)+ext)
+    request.node.add_marker(pytest.mark.internal_path(tempPath), False)
+    #Copy file
+    copyfile(origPath, tempPath)
+    #Wait for test to finish
+    yield
+    #Remove temporary file
+    os.remove(tempPath)
+# ########################################################################### #
+
+# --------------------------------------------------------------------------- #
+def generateRandom(length, chars=string.hexdigits):
+    '''Generate a random string of given length, not cryptographically secure'''
+    return ''.join(random.choice(chars) for _ in range(length))
+# ########################################################################### #
+
+# --------------------------------------------------------------------------- #
+@pytest.fixture
+def dbCon(request):
+    '''Get a database connection to the database given via the "internal_path" marker'''
+    #Read database path
+    marker = request.node.get_closest_marker("internal_path")
+    dbPath = marker.args[0]
+    #Open database
+    _dbCon = sqlite3.connect(dbPath)
+    yield _dbCon
+    _dbCon.close()
+# ########################################################################### #
+
+# --------------------------------------------------------------------------- #
+@pytest.fixture
+def db(dbCon):
+    '''Get a database cursor to the database given via the "internal_path" marker'''
+    return dbCon.cursor()
 # ########################################################################### #
